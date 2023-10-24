0c7d6feb3f8eb4e6874eb8d15374c45616d469d3
quoc loc <loc842211@gmail.com>
2023-10-24 19:55:44 +0700

    hashpassword

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,12 +1,4 @@
-import mysql from 'mysql2';
-
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'jwt'
-});
-
+import userService from '../service/userService'
 const handleHelloword = (req,res)=>{
     return res.render("home.ejs")//render la dung view
 }
@@ -18,16 +10,9 @@ const handleCreateNewUser=(req,res)=>{
     let password=req.body.password
     let username=req.body.username
 
-    connection.query(
-        ' INSERT INTO users (email, password, username) VALUES (?, ?, ?)', [email,password,username],
-        
-        function(err, results, fields) {
-          if(err){
-            console.log(err)
-          }
-        }
-      );
-      
+    userService.getUserList()
+    //userService.createNewUser(email,password,username)
+
     return res.send('sss')
 }
 module.exports= {
---
+++ src/service/userService.js
@@ -0,0 +1,49 @@
+import bcrypt from 'bcryptjs'
+import mysql from 'mysql2';
+
+const salt = bcrypt.genSaltSync(10)
+
+// create the connection to database
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt'
+  });
+  
+
+const hashUserPassword = (userPassword) => {
+    let hashPassword = bcrypt.hashSync(userPassword,salt) //bawm password
+    return hashPassword
+}
+
+const createNewUser=(email,password,username)=>{
+    let hashPass=hashUserPassword(password)
+
+    
+    connection.query(
+        ' INSERT INTO users (email, password, username) VALUES (?, ?, ?)', [email,hashPass,username],
+        
+        function(err, results, fields) {
+          if(err){
+            console.log(err)
+          }
+        }
+      );
+      //tao moi user
+}
+const getUserList =()=>{
+    let users=[];
+    connection.query(
+        ' Select * from users ',
+        function(err, results, fields) {
+          if(err){
+            console.log(err)
+          }
+          console.log('check results',results)
+        }
+      );
+}
+module.exports={
+    createNewUser,getUserList
+}
